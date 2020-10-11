98b1e746a79be8e06694a97102f27b854bf5943c
jsj0828 <wjdtjdwlsqkqn@gmial.com>
2020-10-12 00:52:22 +0900

    jacob 추가

--- src/main/java/com/stock/my/contoller/WelcomeController.java
+++ src/main/java/com/stock/my/contoller/WelcomeController.java
@@ -1,13 +1,23 @@
 package com.stock.my.contoller;
 
+import com.jacob.activeX.ActiveXComponent;
+import com.jacob.com.Dispatch;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WelcomeController {
 
+    private static ActiveXComponent activeXComponent;
+    private static Dispatch dispatch;
+
     @RequestMapping("/welcome")
     public String welcome() {
+        // activeX component name or clsId
+        String clsId = "KHOpenAPI.ocx";
+        activeXComponent = new ActiveXComponent("KHOpenAPI.ocx");
+
+
         return "welcome";
     }
 }
--- src/main/java/com/stock/my/loadDLL/LoadDLL.java
+++ src/main/java/com/stock/my/loadDLL/LoadDLL.java
@@ -1,8 +1,13 @@
 package com.stock.my.loadDLL;
 
+import com.jacob.activeX.ActiveXComponent;
+import com.jacob.com.Dispatch;
 import org.springframework.stereotype.Component;
 
 @Component
 public class LoadDLL {
+    private static ActiveXComponent activeXComponent;
+    private static Dispatch dispatch;
+
 
 }
